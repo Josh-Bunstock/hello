b5c1fc47c4b121e92d0a39dfbb262aa6d256aea2
Josh Bunstock <jbunstock4@huskers.unl.edu>
2021-08-26 13:57:10 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Josh Bunstock
+ *         jbunstock@unl.edu
+ * Date: 2021/08/26
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Josh Bunstock\n");
+
+  return 0;
+}
